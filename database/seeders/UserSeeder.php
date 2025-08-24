ce8b701e7ff295bded83d2c3d67d4005b52c122f
almunawar12 <rifkialmunawra34@gmail.com>
2025-08-24 19:34:46 +0700

    create seed db

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'roles' => 'ADMIN',
+            'email_verified_at' => now(),
+        ]);
+
+        User::create([
+            'name' => 'Regular User',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password'),
+            'roles' => 'USER',
+            'email_verified_at' => now(),
+        ]);
+    }
+}
